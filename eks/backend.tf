652c308f750b056bd6da488fe4ae70048d1dc1ce
Sai Vidyadhar <106044824+Vidyadhar-R@users.noreply.github.com>
2024-08-12 23:14:15 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-aws-bucket-7"
+    bucket         = "my-aws-bucket-77"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
